c96ac9ff59789e9717b359df90d8ca003cf5face
sreeragmmundayadan <42163718+sreeragmmundayadan@users.noreply.github.com>
2022-05-27 12:44:16 +0530

    tdd calculator

---
+++ tdd.js
@@ -0,0 +1,20 @@
+  calculate(numberString = '', delimiter = ',') {
+        let sum = 0;
+
+        this.convertString(numberString, delimiter).forEach((number) => {
+            this.validateNumber(number);
+            sum += +number;
+        });
+
+        return sum;
+    }
+
+    convertString(string, delimiter) {
+        return string.split(delimiter);
+    }
+
+    validateNumber(number) {
+        if(number < 0) {
+            throw Error('negatives not allowed');
+        }
+    }
